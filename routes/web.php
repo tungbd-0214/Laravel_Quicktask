46bd50db70e9913ca7586f3b56f7505542f82298
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2023-08-27 00:23:16 +0700

    chapter 6: create and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/users', UserController::class);
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+
+Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.delete');
