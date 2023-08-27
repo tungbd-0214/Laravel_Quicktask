7e1c51b138e929673ed98b98909fce85337491da
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2023-08-28 01:39:23 +0700

    Use authenication

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +17,7 @@ use App\Http\Controllers\TaskController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::middleware(['admin'])->group(function () {
 	Route::get('/users', [UserController::class, 'index'])
 	    		->name('users.index');
@@ -47,4 +47,14 @@ Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
 Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 
-Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
