c9356e876d88e43a5272ba607dad5eee64377065
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2023-08-30 00:09:54 +0700

    create simple UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -15,24 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+        return view('welcome');
 });
 
 Route::middleware(['admin'])->group(function () {
-	Route::get('/users', [UserController::class, 'index'])
-	    		->name('users.index');
-	Route::get('/users/create', [UserController::class, 'create'])
-				->name('users.create');
-	Route::get('/users/{user}/edit', [UserController::class, 'edit'])
-				->name('users.edit');
-	Route::get('/users/{user}', [UserController::class, 'show'])
-				->name('users.show');
-	Route::post('/users', [UserController::class, 'store'])
-				->name('users.store');
-	Route::put('/users/{user}', [UserController::class, 'update'])
-				->name('users.update');
-	Route::delete('/users/{user}', [UserController::class, 'delete'])
-				->name('users.delete');
+    Route::get('/users', [UserController::class, 'index'])
+        ->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])
+        ->name('users.create');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])
+        ->name('users.edit');
+    Route::get('/users/{user}', [UserController::class, 'show'])
+        ->name('users.show');
+    Route::post('/users', [UserController::class, 'store'])
+        ->name('users.store');
+    Route::put('/users/{user}', [UserController::class, 'update'])
+        ->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'delete'])
+        ->name('users.delete');
 });
 
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
